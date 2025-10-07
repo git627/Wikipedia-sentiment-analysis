c00e857c2791da07ae811cef036450a028317a54
git627 <164899835+git627@users.noreply.github.com>
2025-10-06 21:04:33 -0400

    adding correlation plotting capability

--- Code/sentiment_plots.py
+++ Code/sentiment_plots.py
@@ -161,6 +161,9 @@ def plot_individual(models,input_filename,input_path=None,output_filename=
 
     sentiments=['Negative','Neutral','Positive']
     
+    if type(models)==str:
+        models=[models]
+    
     models=list(map(lambda x: model_name(x), models))
     
     if input_path!=None:
@@ -210,6 +213,9 @@ def plot_group(models,input_filename,label_cols,input_path=None,output_filename=
                label_values=None,min_group_size=None):
      
     sentiments=['Negative','Neutral','Positive']
+    
+    if type(models)==str:
+        models=[models]
     
     models=list(map(lambda x: model_name(x), models))
     
@@ -290,3 +296,117 @@ def plot_group(models,input_filename,label_cols,input_path=None,output_filename=
     if save_fig==True:
         plt.savefig(output_filename, bbox_inches = 'tight')
     
+def plot_corrs(models,input_filename,x_col,input_path=None,output_filename='figure.png',
+              output_path=None,save_fig=True,plot_metric='paragraph_percent',
+              filter_cols=None,filter_values=None):
+
+    sentiments=['Negative','Neutral','Positive']
+    
+    if type(models)==str:
+        models=[models]
+    
+    models=list(map(lambda x: model_name(x), models))
+    
+    if input_path!=None:
+        os.chdir(input_path)
+    
+    df=pd.read_csv(input_filename)
+    
+    if filter_cols!=None:
+    
+        combos_orig=df[filter_cols]
+        combos_filtered=combos_orig
+        
+        for i in range(len(filter_cols)):
+            combos_filtered=combos_filtered[combos_filtered[filter_cols[i]].isin(filter_values[i])]
+                
+        inds_match=combos_filtered.index
+        df=df.loc[inds_match]        
+        df=df.reset_index(drop=True)
+    
+    x_vals=df[x_col].to_numpy()
+    
+    corr_metrics=['paragraph_percent','article_prob']
+    
+    if plot_metric not in corr_metrics:
+        raise ValueError('Please select plot_metric from the following values: {}'.format(corr_metrics))
+        sys.exit(1)
+        
+    title_str,_,plot_dict=globals()[plot_metric](models,df)
+    
+    plot_array_dict={}
+    
+    for i in range(len(models)):
+        plot_array_dict[models[i]]=np.empty((df.shape[0],3))
+        for j in range(df.shape[0]):
+            plot_array_dict[models[i]][j]=plot_dict[models[i]][j]
+            
+    fig, axs = plt.subplots(nrows=len(models),ncols=3,figsize=(18, 4*len(models)))
+        
+    corrs=np.empty((len(models),3))
+    
+    if min(x_vals)>=0:
+        min_x_plot=0.95*min(x_vals)
+    else:
+        min_x_plot=1.05*min(x_vals)
+        
+    if max(x_vals)>=0:
+        max_x_plot=1.05*max(x_vals)
+    else:
+        max_x_plot=0.95*max(x_vals)
+        
+    if max_x_plot<=min_x_plot:
+        if min_x_plot>=0:
+            max_x_plot=1.05*min_x_plot
+        else:
+            max_x_plot=0.95*min_x_plot
+            
+    if plot_metric=='paragraph_percent':
+        y_lim_max=100
+    elif plot_metric=='article_prob':
+        y_lim_max=1
+            
+    y_coord=-0.06*len(models)+1.16
+        
+    for i in range(len(models)):
+        for j in range(3):
+            count_low=len(np.where(plot_array_dict[models[i]][:,j]<=0.5*y_lim_max)[0])
+            count_high=len(np.where(plot_array_dict[models[i]][:,j]>0.5*y_lim_max)[0])
+            if count_high>count_low:
+                text_height=0.15
+            else:
+                text_height=0.85
+            if len(models)>1:
+                corrs[i,j]=np.corrcoef(x_vals,plot_array_dict[models[i]][:,j])[0,1] #Pearson correlations
+                axs[i,j].plot(x_vals,plot_array_dict[models[i]][:,j],'b.')
+                axs[i,j].set_xlim(min_x_plot,max_x_plot)
+                axs[i,j].set_ylim(0,y_lim_max)
+                axs[i,j].text(0.45*(max(x_vals)+min(x_vals)),text_height*y_lim_max,'$r^2$ = {}'.format(np.round(corrs[i,j]**2,2)),
+                              fontsize=18)
+                axs[i,j].tick_params(axis='x', labelsize=14)
+                axs[i,j].tick_params(axis='y', labelsize=14)
+                if i==0:
+                    axs[i,j].set_title(sentiments[j], fontsize=22)
+                if j==0:
+                    axs[i,j].set_ylabel(models[i],fontsize=22)
+            else:
+                corrs[i,j]=np.corrcoef(x_vals,plot_array_dict[models[i]][:,j])[0,1] #Pearson correlations
+                axs[j].plot(x_vals,plot_array_dict[models[i]][:,j],'b.')
+                axs[j].set_xlim(min_x_plot,max_x_plot)
+                axs[j].set_ylim(0,y_lim_max)
+                axs[j].text(0.45*(max(x_vals)+min(x_vals)),text_height*y_lim_max,'$r^2$ = {}'.format(np.round(corrs[i,j]**2,2)),
+                              fontsize=18)
+                axs[j].tick_params(axis='x', labelsize=14)
+                axs[j].tick_params(axis='y', labelsize=14)
+                if i==0:
+                    axs[j].set_title(sentiments[j], fontsize=22)
+                if j==0:
+                    axs[j].set_ylabel(models[i],fontsize=22)
+    
+    fig.suptitle('{} vs. {}, n = {}'.format(title_str,x_col,len(df)), fontsize=28,y=y_coord)
+        
+    if output_path!=None:
+        os.chdir(output_path)
+    
+    if save_fig==True:
+        plt.savefig(output_filename, bbox_inches = 'tight')
--- Code/wiki_analysis.py
+++ Code/wiki_analysis.py
@@ -6,6 +6,99 @@ import numpy as np
 import pandas as pd
 import os
 
+def analyze_url(url,models,output_filename='analysis.csv',output_path=None,show_progress=True):
+    char_limit=2000 #character limit, used to avoid running up against model token limits
+    min_para_len=50 #minimum string length for paragraphs. Used to filter out headings
+        
+    if type(models)==str:
+        models=[models]
+    
+    model_objs=[]
+        
+    module=__import__('sentiment_models')
+    for k in range(len(models)):
+        class_ = getattr(module, models[k])
+        model_objs.append(class_())
+    
+    paragraphs=get_wikipedia_paragraphs_with_api(url)
+    paragraphs_filtered = [s for s in paragraphs if len(s) >= min_para_len]
+    para_lengths = [len(s) for s in paragraphs_filtered]
+    
+    sentiments_dict={}
+    argmax_dict={}
+    probs_dict={}
+    
+    for k in range(len(models)):
+        sentiments_dict[k]=[]
+        argmax_dict[k]=np.zeros((len(paragraphs_filtered),3))
+    
+    i=0
+    
+    while i < len(paragraphs_filtered):
+        
+        #Split paragraph in half if it exceeds the character length limit
+        if len(paragraphs_filtered[i])>char_limit:
+            while len(paragraphs_filtered[i])>char_limit:
+                par_half1,par_half2=split_by_closest_period(paragraphs_filtered[i])
+                paragraphs_filtered=paragraphs_filtered[0:i]+[par_half1,par_half2]+paragraphs_filtered[i+1:len(paragraphs_filtered)]
+                para_lengths=para_lengths[0:i]+[len(par_half1),len(par_half2)]+para_lengths[i+1:len(para_lengths)]
+                
+                for k in range(len(models)):
+                    argmax_dict[k]=np.append(argmax_dict[k],np.zeros((1,3)),axis=0)
+        
+        for k in range(len(models)):
+            probs=model_objs[k].predict(paragraphs_filtered[i])              
+            predicted_class_id=id_to_sentiment(np.argmax(probs))
+            sentiments_dict[k].append(predicted_class_id)
+            argmax_dict[k][i,np.argmax(probs)]=1
+            
+            if i==0:
+                probs_dict[k]=probs.reshape(1,-1)
+            else:
+                probs_dict[k]=np.append(probs_dict[k],probs.reshape(1,-1),axis=0)
+        
+        if show_progress==True:
+            print("{}/{} paragraphs analyzed".format(i+1,len(paragraphs_filtered)))
+            
+        i+=1
+        
+    m1_dict={}
+    m2_dict={}
+    m3_dict={}
+    m4_dict={}
+    
+    header_lst=['Name','URL','Article length (characters)']
+    preds_lst=[url[30:len(url)].replace("_"," "),url,sum(para_lengths)]
+    
+    for k in range(len(models)):
+        m1_dict[k]=Method1(sentiments_dict[k])
+        m2_dict[k]=Method2(argmax_dict[k],para_lengths)
+        m3_dict[k]=Method3(probs_dict[k],para_lengths)
+        m4_dict[k]=Method4(argmax_dict[k])
+        header_lst=header_lst+['Most common sentiment, {}'.format(model_name(models[k])),
+                          'Sentiment with highest weighted vote, {}'.format(model_name(models[k])),
+                          'Sentiment with highest weighted probability, {}'.format(model_name(models[k])),
+                          'Weighted probability of negative sentiment, {}'.format(model_name(models[k])),
+                          'Weighted probability of neutral sentiment, {}'.format(model_name(models[k])),
+                          'Weighted probability of positive sentiment, {}'.format(model_name(models[k])),
+                          'Number of paragraphs with negative sentiment, {}'.format(model_name(models[k])),
+                          'Number of paragraphs with neutral sentiment, {}'.format(model_name(models[k])),
+                          'Number of paragraphs with positive sentiment, {}'.format(model_name(models[k])),
+                          'Percent of paragraphs with negative sentiment, {}'.format(model_name(models[k])),
+                          'Percent of paragraphs with neutral sentiment, {}'.format(model_name(models[k])),
+                          'Percent of paragraphs with positive sentiment, {}'.format(model_name(models[k]))]
+        preds_lst=preds_lst+[m1_dict[k],m2_dict[k],m3_dict[k][0],
+                             m3_dict[k][1][0,0],m3_dict[k][1][0,1],m3_dict[k][1][0,2],
+                             m4_dict[k][0][0,0],m4_dict[k][0][0,1],m4_dict[k][0][0,2],
+                             100*m4_dict[k][1][0,0],100*m4_dict[k][1][0,1],100*m4_dict[k][1][0,2]]
+        
+    df_out=pd.DataFrame([preds_lst],columns=header_lst)
+    
+    if output_path!=None:
+        os.chdir(output_path)
+    
+    df_out.to_csv(output_filename,index=False)
+    
 def analyze_list(urls,models,output_filename='analysis.csv',output_path=None,show_progress=True):
         char_limit=2000 #character limit, used to avoid running up against model token limits
         min_para_len=50 #minimum string length for paragraphs. Used to filter out headings
@@ -68,7 +161,7 @@ def analyze_list(urls,models,output_filename='analysis.csv',output_path=None,sho
             m3_dict={}
             m4_dict={}
             
-            header_lst=['Article length']
+            header_lst=['Article length (characters)']
             preds_lst=[sum(para_lengths)]
             
             for k in range(len(models)):
@@ -183,7 +276,7 @@ def analyze_csv(input_filename,models,output_filename='analysis.csv',input_path=
             m3_dict={}
             m4_dict={}
             
-            header_lst=['Article length']
+            header_lst=['Article length (characters)']
             preds_lst=[sum(para_lengths)]
             
             for k in range(len(models)):
@@ -226,4 +319,3 @@ def analyze_csv(input_filename,models,output_filename='analysis.csv',input_path=
             os.chdir(output_path)
         
         df_final.to_csv(output_filename,index=False)
-    
